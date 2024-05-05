6fb95a3350b99222508efb1dfcaef355d472a3de
DEV ARTSID <you@example.com>
2024-05-05 15:00:42 +0300

    Добавление файлов!

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 // TODO: write code here
+const gameBoard = document.getElementById("game-board");
+const cells = Array.from({ length: 16 }, () => document.createElement("div"));
+const character = document.createElement("img");
+
+character.src = "goblin.png";
+character.classList.add("character");
+
+cells.forEach((cell, index) => {
+  cell.classList.add("cell");
+  cell.dataset.index = index;
+  gameBoard.appendChild(cell);
+});
+
+function moveCharacter() {
+  const currentCell = document.querySelector(".cell .character");
+  if (currentCell) {
+    currentCell.remove();
+  }
+
+  const randomCell = cells[Math.floor(Math.random() * cells.length)];
+  randomCell.appendChild(character);
+}
+
+setInterval(moveCharacter, 1000);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
